f95066ed9b24d6f2a281e24893160e8c3f9baf1c
sohamk990 <sohamk990@gmail.com>
2022-06-06 00:02:23 +0530

    Free Translation

--- lib/api/translation_api.dart
+++ lib/api/translation_api.dart
@@ -7,7 +7,7 @@ import 'package:translator/translator.dart';
 class TranslationApi {
   static final _apiKey = 'AIzaSyCKg-8KXrMIryYwif8C4gEFSu4aKXLK3as';
 
-  static Future<String> translate(String message, String toLanguageCode) async {
+  static Future<String> translate2(String message, String toLanguageCode) async {
     final response = await http.post(
       'https://translation.googleapis.com/language/translate/v2?target=$toLanguageCode&key=$_apiKey&q=$message',
     );
@@ -23,7 +23,7 @@ class TranslationApi {
     }
   }
 
-  static Future<String> translate2(
+  static Future<String> translate(
       String message, String fromLanguageCode, String toLanguageCode) async {
     final translation = await GoogleTranslator().translate(
       message,
